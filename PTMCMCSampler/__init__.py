19646122a96d501e573cdd861be4d7ccaa3aee47
Oleg Arenz <arenz@ias.informatik.tu-darmstadt.de>
2018-05-08 14:06:20 +0200

    removed obsolete instructions

--- PTMCMCSampler/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function, division, absolute_import
-
-__version__ = 2015.02
-
-from PTMCMCSampler import *
-
-def test():
-    # Run some tests here
-    print("{0} tests have passed".format(0))
